d0df2fe11e9f20ef20b4da6b16207d63ba0f384e
OliverCanet <113703934+OliverCanet@users.noreply.github.com>
2022-11-23 18:23:36 -0300

    login/register
    El js,css y html de login/register

---
+++ src/main/resources/web/scripts/login-register.js
@@ -0,0 +1,36 @@
+
+
+const { createApp } = Vue
+
+
+let app = createApp({
+    data() {
+        return{
+           active: 'Log In',
+           email: '',
+           password: '',
+           firstName: '',
+           lastName: '',
+            
+
+        }
+    },
+    created(){
+        
+
+
+    },
+    methods:{
+       
+        
+        
+
+    },
+    computed:{
+        
+
+    },
+
+})
+
+app.mount('#app')
