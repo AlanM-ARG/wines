c3992366e4cf802b029267943c7c6e95354add46
Mario <marioarg421@gmail.com>
2022-11-24 18:49:51 -0300

    Creado el controlador ordenes de compra

--- src/main/java/com/ecommerce/wines/controllers/PurchaseOrderController.java
+++ src/main/java/com/ecommerce/wines/controllers/PurchaseOrderController.java
@@ -1,9 +1,44 @@
 package com.ecommerce.wines.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.ecommerce.wines.DTOS.PurchaseOrderDTO;
+import com.ecommerce.wines.models.Client;
+import com.ecommerce.wines.models.PaymentMethod;
+import com.ecommerce.wines.models.Product;
+import com.ecommerce.wines.models.PurchaseOrder;
+import com.ecommerce.wines.services.PurchaseOrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.websocket.server.PathParam;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Set;
 
 @RestController
 @RequestMapping("/api")
 public class PurchaseOrderController {
+
+    @Autowired
+    PurchaseOrderService purchaseOrderService;
+
+    @GetMapping("/purchaseOrder")
+    public List<PurchaseOrderDTO> getPurchaseOrder(){
+        return purchaseOrderService.getPurchaseOrder();
+    }
+
+
+    @PostMapping("/purchaseOrder/create")
+    public ResponseEntity<?> createPurchaseOrder(@RequestParam Client client,
+                                                 @RequestParam double mount,
+                                                 @RequestParam LocalDateTime date,
+                                                 @RequestParam PaymentMethod method){
+
+        PurchaseOrder purchaseOrder = new PurchaseOrder(client, mount, date, method);
+        purchaseOrderService.savePurchaseOrder(purchaseOrder);
+
+        return new ResponseEntity<>("Purchase order create", HttpStatus.CREATED);
+
+    }
 }
--- src/main/java/com/ecommerce/wines/services/PurchaseOrderService.java
+++ src/main/java/com/ecommerce/wines/services/PurchaseOrderService.java
@@ -1,4 +1,12 @@
 package com.ecommerce.wines.services;
 
+import com.ecommerce.wines.DTOS.PurchaseOrderDTO;
+import com.ecommerce.wines.models.PurchaseOrder;
+
+import java.util.List;
+
 public interface PurchaseOrderService {
+    public List<PurchaseOrderDTO> getPurchaseOrder();
+
+    public void savePurchaseOrder(PurchaseOrder purchaseOrder);
 }
