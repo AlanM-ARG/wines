eaf406bb9f4843ad0f74fad5499f1b847a59c209
dariobartoli <113705473+dariobartoli@users.noreply.github.com>
2022-11-25 12:20:20 -0300

    modifique los enum a constantes y cambie los datos de winesaplication

--- src/main/java/com/ecommerce/wines/configurations/WebAuthorization.java
+++ src/main/java/com/ecommerce/wines/configurations/WebAuthorization.java
@@ -1,6 +1,7 @@
 package com.ecommerce.wines.configurations;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -19,7 +20,8 @@ public class WebAuthorization extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
 
         http.authorizeRequests()
-                .antMatchers("/**").permitAll();
+                .antMatchers("/**").permitAll()
+                .antMatchers(HttpMethod.POST).permitAll();
 
 
         http.formLogin()
