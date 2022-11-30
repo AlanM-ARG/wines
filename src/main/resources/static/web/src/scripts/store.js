665d05b52c3978e27ea8d5e3a8a4616ad80e3e22
MatiasG0nzalez <106191565+MatiasG0nzalez@users.noreply.github.com>
2022-11-30 10:54:31 -0300

    acomode carpetas

--- src/main/resources/static/web/src/scripts/store.js
+++ src/main/resources/static/web/src/scripts/store.js
@@ -1,15 +1,16 @@
 const app = Vue.createApp({
     data() {
         return {
-            apiProducts: "/api/products"
+            apiProducts: "/api/products",
+            products: [],
         }
     },created() {
-            
+            this.getProducts(this.apiProducts)
     },methods: {
 
         getProducts(api){
             axios.get(api).then(data=>{
-                
+                this.products = data
             })
         }
         
