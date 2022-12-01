2c75e7e2b748e6ccbdb9b9c16b8df60473028b60
MatiasG0nzalez <106191565+MatiasG0nzalez@users.noreply.github.com>
2022-12-01 04:32:37 -0300

    a

--- src/main/resources/static/web/src/scripts/store.js
+++ src/main/resources/static/web/src/scripts/store.js
@@ -1,56 +1,75 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            apiProducts: "/api/products",
-            products: [],
-            categorys: [],
-            categoryFilter: "",
-        }
-    },created() {
-            this.getProducts(this.apiProducts)
-            console.log("funciona");
-           
-    },methods: {
-
-        getProducts(api){
-            axios.get(api).then(data=>{
-                console.log(data)
-                this.products = data.data
-                this.getCategorys(this.products)
-                console.log(this.categorys);
-            })
-        },getCategorys(array){
-            let categorias = []
-
-            array.forEach(element => {
-                categorias.push(element.category)
-            });
-
-            let categorias1 = new Set(categorias)
-            let categorias2 = [...categorias1]
-            
-            this.categorys = categorias2
-
-        },getVariertys(array){
-            let categorias = []
-
-            array.forEach(element => {
-                categorias.push(element.category)
-            });
-
-            let categorias1 = new Set(categorias)
-            let categorias2 = [...categorias1]
-            
-            this.categorys = categorias2
-
-        },wine(wineCategory){
-            if(wineCategory == "WHITE" || wineCategory == "RED"  || wineCategory == "ROSE"){
-                return wineCategory + " WINE"
-            }
-            else return wineCategory
-        }  
-        
-    },computed: {
-        
+  data() {
+    return {
+      apiProducts: "/api/products",
+      products: [],
+      filteredProducts: [],
+      categorys: [],
+      categoryFilter: "",
+    };
+  },
+  created() {
+    this.getProducts(this.apiProducts);
+    console.log("funciona");
+  },
+  methods: {
+    getProducts(api) {
+      axios.get(api).then((data) => {
+        console.log(data);
+        this.products = data.data;
+        this.filteredProducts = this.products
+        this.getCategorys(this.products);
+        console.log(this.categorys);
+      });
     },
-}).mount("#app")
+    getCategorys(array) {
+      let categorias = [];
+
+      array.forEach((element) => {
+        categorias.push(element.category);
+      });
+
+      let categorias1 = new Set(categorias);
+      let categorias2 = [...categorias1];
+
+      this.categorys = categorias2;
+    },
+    getVariertys(array) {
+      let categorias = [];
+
+      array.forEach((element) => {
+        categorias.push(element.category);
+      });
+
+      let categorias1 = new Set(categorias);
+      let categorias2 = [...categorias1];
+
+      this.categorys = categorias2;
+    },
+    wine(wineCategory) {
+      if (
+        wineCategory == "WHITE" ||
+        wineCategory == "RED" ||
+        wineCategory == "ROSE"
+      ) {
+        return wineCategory + " WINE";
+      } else return wineCategory;
+    }
+
+
+
+  },
+  computed: {
+    productFilter(){
+
+        if(categoryFilter.length && categoryFilter != "ALL"){
+
+            this.filteredProducts = this.products.filter( product => product.category == this.categoryFilter)
+
+
+        }else{
+            this.filteredProducts = this.products
+        }
+    },
+  },
+}).mount("#app");
