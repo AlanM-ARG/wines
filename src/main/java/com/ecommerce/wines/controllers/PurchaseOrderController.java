875455dd1f2fbf94d0e4265d0211a629665f5594
dariobartoli <113705473+dariobartoli@users.noreply.github.com>
2022-12-01 00:04:18 -0300

    endpoint para obtener las ordenes de compra del cliente autenticado

--- src/main/java/com/ecommerce/wines/controllers/PurchaseOrderController.java
+++ src/main/java/com/ecommerce/wines/controllers/PurchaseOrderController.java
@@ -1,5 +1,6 @@
 package com.ecommerce.wines.controllers;
 
+import com.ecommerce.wines.DTOS.FavsDTO;
 import com.ecommerce.wines.DTOS.ProductOrderDTO;
 import com.ecommerce.wines.DTOS.PurchaseDTO;
 import com.ecommerce.wines.DTOS.PurchaseOrderDTO;
@@ -43,6 +44,12 @@ public class PurchaseOrderController {
     @GetMapping("/purchaseorder")
     public List<PurchaseOrderDTO> getPurchaseOrder() {
         return purchaseOrderService.getPurchaseOrder();
+    }
+
+    @GetMapping("/clientcurrent/purchaseorder")
+    public List<PurchaseOrderDTO> getPurchaseOrderClient(Authentication authentication) {
+        Client clientCurrent = clientService.clientFindByEmail(authentication.getName());
+        return clientCurrent.getPurchaseOrders().stream().map(purchaseOrder -> new PurchaseOrderDTO(purchaseOrder)).collect(Collectors.toList());
     }
 
 
