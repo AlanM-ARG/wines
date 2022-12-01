e7e452776036fc1abd39f7f40f0d23359da12c80
MatiasG0nzalez <106191565+MatiasG0nzalez@users.noreply.github.com>
2022-12-01 07:22:46 -0300

    a

--- src/main/resources/static/web/src/scripts/store.js
+++ src/main/resources/static/web/src/scripts/store.js
@@ -88,10 +88,10 @@ const app = Vue.createApp({
         return filteredProducts 
     },filterPrice(array){
         let filteredProducts = []
-        if(this.filterData.priceMax <=0){
+        if(this.filterData.priceMax >0){
             filteredProducts = array.filter( producto => {producto.price >= this.filterData.priceMin && producto.price <= this.filterData.priceMax})
 
-        }else{
+        }else if(this.filterData.priceMax <=0){
             filteredProducts = array
         }
 
