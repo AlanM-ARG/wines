41b36a7fd516ba54805caa31fee257af5cecdb79
MatiasG0nzalez <106191565+MatiasG0nzalez@users.noreply.github.com>
2022-12-01 10:04:19 -0300

    a

--- src/main/resources/static/web/src/scripts/login-register.js
+++ src/main/resources/static/web/src/scripts/login-register.js
@@ -8,7 +8,8 @@ let app = createApp({
             password: '',
             firstName: '',
             lastName: '',
-            url: ''
+            url: '',
+            title: "",
         }
     },
     created() {
