0d54235abc2ceb8b09934be3c14f7d5e140d17c4
dariobartoli <113705473+dariobartoli@users.noreply.github.com>
2022-12-02 00:13:20 -0300

    agregue funcionalidades

--- src/main/resources/static/web/src/index.js
+++ src/main/resources/static/web/src/index.js
@@ -1,13 +1,25 @@
-Vue.createApp({
-  data() {
-    return {
-      nombre: "",
-      
-    };
+const app = Vue.createApp({
+  data(){
+      return{
+          url: "http://localhost:8080/api/clients/current",
+          client: "",
+          clientUp: "",
+      }
   },
+  created(){
+      this.getdata()
+  },
+  methods:{
+      getdata(){
+          axios.get(this.url)
+          .then(data => {
+            this.client = data.data.firstName
+            this.clientUp = data.data.firstName.toUpperCase()
+            console.log(this.client)
 
-  methods: {
-    
+          })
+      },
   },
-}).mount("#app");
+})
+app.mount("#app")
 
