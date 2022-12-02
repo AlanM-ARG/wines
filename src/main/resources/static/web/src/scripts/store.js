9909482e6b96f102ef4974b6fb15e259c31bb45a
MatiasG0nzalez <106191565+MatiasG0nzalez@users.noreply.github.com>
2022-12-02 06:14:41 -0300

    checkout

--- src/main/resources/static/web/src/scripts/store.js
+++ src/main/resources/static/web/src/scripts/store.js
@@ -2,6 +2,7 @@ const app = Vue.createApp({
   data() {
     return {
       apiProducts: "/api/products",
+      apiFavorites: "/api/clients/favs",
       products: [],
       filteredProducts: [],
       categorys: [],
@@ -11,6 +12,10 @@ const app = Vue.createApp({
         priceMax: 0,
       },
       favorites: [],
+      cart: [],
+      subtotal:0,
+      total: 0,
+      items:0,
     };
   },
   created() {
@@ -25,6 +30,13 @@ const app = Vue.createApp({
         this.filteredProducts = this.products;
         this.getCategorys(this.products);
         console.log(this.categorys);
+      });
+    },
+    getFavorites(api){
+      axios.get(api).then((data) => {
+        
+        this.favorites = data.data;
+        
       });
     },
     getCategorys(array) {
@@ -106,20 +118,104 @@ const app = Vue.createApp({
 
       return filteredProducts;
     },
-    addFavorite() {
+    addFavorite(name) {
 
 
-      /* this.favorites.push(producto); */
+      axios.post(`/api/clients/favs?name=${name}`).then(() =>Swal.fire({
+        position: 'top-end',
+        icon: 'success',
+        title: 'Product added to favorites',
+        showConfirmButton: false,
+        timer: 1000
+      })
+      ).catch(error =>  Swal.fire(
+        'Oops!',
+        error.response.data,
+        'warning'
+      ))
 
-      Swal.fire(
-        'Added to cart',
-        ':)',
-        'success'
-      )
-    },
+
+  },addToCart(product){
 
 
-  },
+    
+    let existe = this.cart.some( item => item.id == product.id)
+
+    if(existe){
+
+      this.cart.forEach(producto =>{
+
+        if(producto.id == product.id){
+
+          producto.quantity++
+          
+          if(producto.quantity > producto.stock){
+
+            producto.quantity = producto.stock
+
+            Swal.fire(
+              'Out of stock',
+              'You have reached the limit of available stock',
+              'warning'
+            )
+
+          }else{
+
+            Swal.fire({
+              position: 'top-end',
+              icon: 'success',
+              title: 'Product added to cart!',
+              showConfirmButton: false,
+              timer: 1000
+            })
+
+          }
+        }
+
+      })
+
+
+    }
+    else{
+    this.cart.push({
+      id:product.id,
+      category: product.category,
+      variety: product.variety,
+      name:product.name,
+      image: product.image,
+      price:product.price,
+      quantity: 1,
+      stock: product.stock,
+    })
+    
+    Swal.fire({
+  position: 'top-end',
+  icon: 'success',
+  title: 'Product added to cart!',
+  showConfirmButton: false,
+  timer: 1000
+})
+    this.saveCart()
+  }
+
+    
+    
+
+    
+
+  },removeFromCart(product){
+    let newCart = this.cart.filter(producto => producto.id != product.id)
+
+    this.cart = newCart
+    this.saveCart()
+  },saveCart(){
+    localStorage.setItem('cart',JSON.stringify(this.cart))
+  
+
+
+
+
+}},
   computed: {
     filterFinal() {
       if (
@@ -131,6 +227,41 @@ const app = Vue.createApp({
           this.filterName(this.filterCategory(this.products))
         );
       }
-    },
+    },calculateSomeThings(){
+
+      let subtotal1 = 0
+      let total1 = 0
+      let items1 = 0
+
+      this.cart.forEach(product => {
+        items1 += product.quantity
+        subtotal1 += product.price * product.quantity
+        
+
+
+        
+
+      })
+      if(subtotal1 != 0){
+          total1 = subtotal1 + 30 + 35
+      }
+      else{total1 = 0}
+      this.subtotal = subtotal1.toFixed(2)
+      this.total = total1.toFixed(2)
+      this.items = items1
+
+
+    }
+
+
+
+  },mounted() {
+
+    let local = JSON.parse(localStorage.getItem('cart'))
+
+    if(local !== null){
+	    this.cart = local
+    }
+    
   },
 }).mount("#app");
